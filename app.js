a1eda720e32d17d402034a78853696935a9da250
= <=>
2024-12-05 09:38:19 +0530

    update cors 05-12-24

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ const app = express();
 app.use(express.json()) // req body se jo data ayega bo json me parse hoke ayega
 app.use(express.urlencoded( {extended: true }));  // url ko encode krne ke liye(query params bgera nikalne me) 
 
-// app.use(cors({
-//     origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
-//     credentials: true
-// }))
+app.use(cors({
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    credentials: true
+}))
 
-app.use(cors({ origin: '*' }));
+// app.use(cors({ origin: '*' }));
 
 app.use(cookieParser()) // cookie ke ander token bgera ko parse karne ke liye
 
