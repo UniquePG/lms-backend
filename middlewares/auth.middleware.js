5ecc76a80b399c6a4542356b9a8ee8f6d4d8273c
= <=>
2024-12-05 10:07:06 +0530

    add log for verify token 05-12-24

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,6 +4,8 @@ import jwt from 'jsonwebtoken';
 const isLoggedIn =  async (req, res, next)=>{
     const { token } = req.cookies;
 
+    console.log("token", token);
+
     if(!token){
         return next(new AppError('Unauthenticated, please login again', 401))
     }
